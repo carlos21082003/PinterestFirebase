2689f9d13b6a0c3c4c126acec0c760e4244b178c
carlos21082003 <oz2003x8@gmail.com>
2025-06-18 17:25:01 -0500

    agregando primera parte del foro

---
+++ app/src/main/java/com/pinterest/pinterestfirebase/ui/foro/ReutilizarActivity.kt
@@ -0,0 +1,21 @@
+package com.pinterest.pinterestfirebase.ui.foro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pinterest.pinterestfirebase.R
+
+class ReutilizarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reutilizar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
